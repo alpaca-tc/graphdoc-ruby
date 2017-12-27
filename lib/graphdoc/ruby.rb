a962de12dc0e73924c361908bac8dd3ed5fabb9a
alpaca-tc <alpaca-tc@alpaca.tc>
2017-12-27 19:14:05 +0900

    Initialize repository

---
+++ lib/graphdoc/ruby.rb
@@ -0,0 +1,7 @@
+require "graphdoc/ruby/version"
+
+module Graphdoc
+  module Ruby
+    # Your code goes here...
+  end
+end
---
+++ lib/graphdoc/ruby/version.rb
@@ -0,0 +1,5 @@
+module Graphdoc
+  module Ruby
+    VERSION = "0.1.0"
+  end
+end
